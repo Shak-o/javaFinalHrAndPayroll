25f290df9fd0de615f1dcf76da215073c2aeb55f
shalvaX <shalva.turashvili.1@btu.edu.ge>
2021-01-04 19:31:25 +0400

    salary

--- src/ge/edu/btu/server/dao/SalaryDAO.java
+++ src/ge/edu/btu/server/dao/SalaryDAO.java
@@ -1,7 +1,9 @@
 package ge.edu.btu.server.dao;
 
+import ge.edu.btu.server.model.Salary;
+
 public interface SalaryDAO {
-    void addSalary();
-    void editSalary();
-    void deleteSalary();
+    void addSalary(Salary salary);
+    void editSalary(Salary salary);
+    void deleteSalary(Salary salary);
 }
--- src/ge/edu/btu/server/dao/SalaryDAOImpl.java
+++ src/ge/edu/btu/server/dao/SalaryDAOImpl.java
@@ -1,19 +1,21 @@
 package ge.edu.btu.server.dao;
 
+import ge.edu.btu.server.model.Salary;
+
 public class SalaryDAOImpl implements SalaryDAO {
 
     @Override
-    public void addSalary() {
+    public void addSalary(Salary salary) {
 
     }
 
     @Override
-    public void editSalary() {
+    public void editSalary(Salary salary) {
 
     }
 
     @Override
-    public void deleteSalary() {
+    public void deleteSalary(Salary salary) {
 
     }
 }
